14b009dd6ea7f79efb15f57d4c722a1be6eff39b
Joe Reed <go0baahh@users.noreply.github.com>
2017-01-17 08:43:49 -0600

    Create Arrays.php

---
+++ Arrays.php
@@ -0,0 +1,21 @@
+/*
+ * Return the values from a single column in the input array
+ * See http://php.net/manual/en/function.array-column.php
+ */
+if(!function_exists("array_column"))
+{
+    function array_column($array, $column_name)
+    {
+        return array_map(function($element) use($column_name){return $element[$column_name];}, $array);
+    }
+}
+
+
+/*
+ * Return if array is multi-dimensional or not 
+ * @Param array.
+ */
+function isMultiArray($a){
+    foreach($a as $v) if(is_array($v)) return TRUE;
+    return FALSE;
+}
