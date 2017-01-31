060d2bb887c379545d87d5eedae274a7f04930f3
Joe Reed <go0baahh@users.noreply.github.com>
2017-01-31 13:05:00 -0600

    Create Strings.php

---
+++ Strings.php
@@ -0,0 +1,7 @@
+
+function getStardate(void)
+{
+ 	$offset = 2000;
+  	$seconds_per_stardate = 31449.6; // is the number of seconds in a year divided by 1000, for hopefully obvious reasons
+  	return time() / $seconds_per_stardate + $offset;
+}
