097922b9cadc2338a942f1543e54e6cb18228ce3
Joe <go0baahh@users.noreply.github.com>
2020-12-03 22:49:20 -0600

    Update Arrays.php

--- Arrays.php
+++ Arrays.php
@@ -1,50 +1,20 @@
 <?php
-/*
- * Return the values from a single column in the input array
- *
+
+/**
+ * Return the values from a column
  */
-if (!function_exists('array_column')) {
-    function array_column($array, $column_name) {
-        return array_map(function($element) use($column_name) {return $element[$column_name];}, $array);
-    }
+function array_column($array, $column_name) {
+    return array_map(function($element) use($column_name) { 
+        return $element[$column_name]; 
+    }, $array);
 }
 
 
-// Example
-$records = [
-    [
-        'id' => 2135,
-        'first_name' => 'John',
-        'last_name' => 'Doe',
-    ],
-    [
-        'id' => 3245,
-        'first_name' => 'Sally',
-        'last_name' => 'Smith',
-    ],
-    [
-        'id' => 5342,
-        'first_name' => 'Jane',
-        'last_name' => 'Jones',
-    ],
-    [
-        'id' => 5623,
-        'first_name' => 'Peter',
-        'last_name' => 'Doe',
-    ]
-];
-
-$first_names = array_column($records, 'first_name');
-print_r($first_names);
-
-
-/*
+/**
  * Return if array is multi-dimensional or not
  */
 function isMultiArray($a) {
     foreach($a as $v) return is_array($v) ? true : false;
 }
-
-print_r(isMultiArray($records));
 
 ?>
